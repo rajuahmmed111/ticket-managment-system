66449881b7c0829bea96903c45c2a42c561e2c40
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-10 17:47:34 +0600

    globar error handler apply in app ts

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import bodyParser from 'body-parser';
 
 import morgan from 'morgan';
 import router from './app/routes';
+import GlobalErrorHandler from './app/middleware/globalErrorHandler';
 
 const app: Application = express();
 
@@ -40,7 +41,7 @@ app.get('/', (req: Request, res: Response) => {
 app.use('/api/v1',router);
 
 // Error handling middleware
-// app.use(GlobalErrorHandler);
+app.use(GlobalErrorHandler);
 
 // 404 Not Found handler
 app.use((req: Request, res: Response, next: NextFunction) => {
