d744f8e3ef59f540464ea444cbd748098436faa4
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-10 23:21:29 +0600

    update initiat ticket route

--- src/app/modules/Ticket/ticket.route.ts
+++ src/app/modules/Ticket/ticket.route.ts
@@ -1,5 +1,9 @@
 import express from "express"
 
-const router = express.Router
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.send('Ticket API is running')
+})
 
 export const ticketRoute = router
