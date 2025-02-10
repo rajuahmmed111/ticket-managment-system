aee28c5474c7476756f92a5c7ff6cc51cb0c006e
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-10 19:50:22 +0600

    update user controller

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import { UserService } from './user.service';
 import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
-import { UserService } from './user.service';
 import ApiError from '../../../errors/ApiErrors';
+
 
 const createUser = catchAsync(async (req: Request, res: Response) => {
   const result = await UserService.createUser(req.body);
