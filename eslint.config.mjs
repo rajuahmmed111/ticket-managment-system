b0ba37294cc5b6210b9d341740fc6034df0fa49f
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-10 16:50:07 +0600

    update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,22 @@
-{
-    "env": {
-      "browser": true,
-      "es2021": true,
-      "jest": true
-    },
-    "extends": [
-      "airbnb",
-      "plugin:prettier/recommended" // Integrates Prettier with ESLint
-    ],
-    "parserOptions": {
-      "ecmaFeatures": {
-        "jsx": true
-      },
-      "ecmaVersion": 12,
-      "sourceType": "module"
-    },
-    "plugins": ["react", "prettier"],
-    "rules": {
-      "prettier/prettier": "error" // Show Prettier errors as ESLint errors
-    }
-  }
+// {
+//     "env": {
+//       "browser": true,
+//       "es2021": true,
+//       "jest": true
+//     },
+//     "extends": [
+//       "airbnb",
+//       "plugin:prettier/recommended" // Integrates Prettier with ESLint
+//     ],
+//     "parserOptions": {
+//       "ecmaFeatures": {
+//         "jsx": true
+//       },
+//       "ecmaVersion": 12,
+//       "sourceType": "module"
+//     },
+//     "plugins": ["react", "prettier"],
+//     "rules": {
+//       "prettier/prettier": "error" // Show Prettier errors as ESLint errors
+//     }
+//   }
