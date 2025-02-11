a5be12a30f4d9800072da4b10d6f9f559cbb1676
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-11 12:38:47 +0600

    update ticket service

--- src/app/modules/Ticket/ticket.controller.ts
+++ src/app/modules/Ticket/ticket.controller.ts
@@ -1,22 +1,23 @@
-import httpStatus from "http-status";
-import catchAsync from "../../../shared/catchAsync";
-import sendResponse from "../../../shared/sendResponse";
-import { ticketService } from "./ticket.service";
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { ticketService } from './ticket.service';
 
 // create ticket
-const createTicket = catchAsync( async(req, res) => {
-    const userId = req.user?.id;
+const createTicket = catchAsync(async (req, res) => {
+  const userId = req.user?.id;
+  const ticketData = req.body;
 
-    const result = await ticketService.createTicket(userId)
+  const result = await ticketService.createTicket(ticketData, userId);
 
-    sendResponse(res, {
-        success: true,
-        statusCode: httpStatus.CREATED,
-        message: 'Ticket created successfully',
-        data: result,
-    });
-})
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.CREATED,
+    message: 'Ticket created successfully',
+    data: result,
+  });
+});
 
 export const ticketController = {
-    createTicket,
-}
+  createTicket,
+};
--- src/app/modules/Ticket/ticket.service.ts
+++ src/app/modules/Ticket/ticket.service.ts
@@ -1,9 +1,22 @@
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiErrors';
 import prisma from '../../../shared/prisma';
 
-const createTicket = async (payload: any) => {
+const createTicket = async (payload: any, userId: string) => {
+  if (!userId) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+  }
+
   const newTicket = await prisma.ticket.create({
     data: payload,
+    include: {
+      user: true,
+    },
+    // where: {
+    //   id: userId,
+    // },
   });
+
   return newTicket;
 };
 
