3dc1774cd4d346a8e33ed28ba635dd041212795b
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-11 19:17:59 +0600

    sending replay in ticket in user

--- src/app/modules/Replay/replay.controller.ts
+++ src/app/modules/Replay/replay.controller.ts
@@ -5,7 +5,7 @@ import { replayService } from './replay.service';
 
 // send replay in ticket ways user
 const sendReplay = catchAsync(async (req, res) => {
-  const userId = req.user?.id;
+  const senderId = req.user?.id;
   const ticketId = req.params.ticketId;
   const { message } = req.body;
 
--- src/app/modules/Replay/replay.service.ts
+++ src/app/modules/Replay/replay.service.ts
@@ -1,8 +1,34 @@
-const sendReplay = async (userId : string, ticketId: string, message: string) => {
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiErrors';
+import prisma from '../../../shared/prisma';
 
-    const 
+const sendReplay = async (
+  senderId: string,
+  ticketId: string,
+  message: string
+) => {
+  if (!senderId) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+  }
+  const ticket = await prisma.ticket.findUnique({
+    where: { id: ticketId },
+    include: {
+      user: {
+        select: { id: true },
+      },
+    },
+  });
+
+  if (!ticket || !ticket.user) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Receiver not found');
+  }
+
+  const receiverId = ticket.user.id;
+
+  const [person1, person2] = [senderId, receiverId].sort();
+  const channelName = person1 + person2;
 };
 
 export const replayService = {
-    sendReplay
-}
+  sendReplay,
+};
