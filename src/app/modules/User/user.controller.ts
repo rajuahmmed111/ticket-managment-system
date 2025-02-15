2e29820e13df4da6f36639c077987baf6fb71ab4
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-15 21:56:40 +0600

    updat user

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -5,7 +5,7 @@ import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
 import ApiError from '../../../errors/ApiErrors';
 
-
+// create  user
 const createUser = catchAsync(async (req: Request, res: Response) => {
   const result = await UserService.createUser(req.body);
 
